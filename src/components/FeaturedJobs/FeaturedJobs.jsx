56985bbe14d135428463cb8958f027cae6d1b36f
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-09-22 23:43:54 +0600

    Featured jobs dynamic data added

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import Job from "../Job/Job";
 
 
 const FeaturedJobs = () => {
+    const[jobs, setJobs] = useState([])
+
+    useEffect (() => {
+        fetch('jobs.json')
+        .then(res => res.json())
+        .then(data => setJobs(data))
+    },[]) 
+
     return (
         <div className="text-center">
             <h1 className="text-5xl">Featured Jobs</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <Job></Job>
+            {
+                jobs.map(job => <Job key={job.id} job={job}></Job>)
+            }
+            
         </div>
         
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,11 +1,30 @@
+import PropTypes from 'prop-types';
 
+const Job = ({job}) => {
 
-const Job = () => {
+    const{logo, job_title, company_name, remote_or_onsite, location, job_type, salary} = job;
+
     return (
-        <div>
-            job section
+        <div className="grid grid-cols-2 border-gray-400">
+            <section>
+                <img src={logo} alt="" />
+                <h2>{job_title}</h2>
+                <h3>{company_name}</h3>
+                <div className="flex">
+                    <button>{remote_or_onsite}</button>
+                    <button>{job_type}</button>
+                </div>
+                <div className="flex">
+                    <p>{location}</p>
+                    <p>Salary: {salary}</p>
+                </div>
+            </section>
         </div>
     );
 };
 
 export default Job;
+
+Job.propTypes ={
+    job: PropTypes.object,
+}
