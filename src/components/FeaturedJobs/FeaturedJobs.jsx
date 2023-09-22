bd621f20e467da5b900311bbf47f831f3e84be17
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-09-23 00:12:52 +0600

    Featured jobs layout added

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,24 +3,26 @@ import Job from "../Job/Job";
 
 
 const FeaturedJobs = () => {
-    const[jobs, setJobs] = useState([])
+    const [jobs, setJobs] = useState([])
 
-    useEffect (() => {
+    useEffect(() => {
         fetch('jobs.json')
-        .then(res => res.json())
-        .then(data => setJobs(data))
-    },[]) 
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [])
 
     return (
         <div className="text-center">
             <h1 className="text-5xl">Featured Jobs</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            {
-                jobs.map(job => <Job key={job.id} job={job}></Job>)
-            }
-            
+            <div className="my-12 grid grid-cols-2 gap-4">
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
+
         </div>
-        
+
     );
 };
 
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -5,19 +5,20 @@ const Job = ({job}) => {
     const{logo, job_title, company_name, remote_or_onsite, location, job_type, salary} = job;
 
     return (
-        <div className="grid grid-cols-2 border-gray-400">
-            <section>
+        <div>
+            <section className='border-2 p-4 rounded-lg space-y-2' >
                 <img src={logo} alt="" />
-                <h2>{job_title}</h2>
-                <h3>{company_name}</h3>
+                <h2 className='text-left'>{job_title}</h2>
+                <h3 className='text-left'>{company_name}</h3>
                 <div className="flex">
-                    <button>{remote_or_onsite}</button>
-                    <button>{job_type}</button>
+                    <button className=' border-2 rounded-md px-5 py-2 font-semibold mr-3'>{remote_or_onsite}</button>
+                    <button className=' border-2 rounded-md px-5 py-2 font-semibold mr-3'>{job_type}</button>
                 </div>
-                <div className="flex">
+                <div className="flex justify-between">
                     <p>{location}</p>
                     <p>Salary: {salary}</p>
                 </div>
+                <button className='btn bg-blue-500 text-white'>View Details</button>
             </section>
         </div>
     );
